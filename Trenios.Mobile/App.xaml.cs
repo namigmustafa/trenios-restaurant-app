643a1d8adb92f3909435335a73c23e0c67a9c2be
Mustafayev, Namig <namig.mustafayev@youforce.net>
2026-01-27 03:22:18 +0200

    animation

--- Trenios.Mobile/App.xaml.cs
+++ Trenios.Mobile/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Trenios.Mobile
+﻿using Trenios.Mobile.Pages;
+
+namespace Trenios.Mobile
 {
     public partial class App : Application
     {
@@ -22,7 +24,8 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            // Show animated splash page first
+            return new Window(new SplashPage());
         }
     }
 }
