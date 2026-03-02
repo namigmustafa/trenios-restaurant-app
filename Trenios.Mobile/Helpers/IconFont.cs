74f849fb1677c4366ad07312776f847b6816fe6c
Mustafayev, Namig <namig.mustafayev@youforce.net>
2026-03-03 00:42:06 +0200

    Add Fluent UI icon font and IconFont helper class
    Added FluentSystemIcons-Regular.ttf to the project and introduced the IconFont static class in Trenios.Mobile.Helpers. IconFont provides named constants for Fluent UI System Icons, mapping icon names to their Unicode glyphs for easier usage throughout the app.

---
+++ Trenios.Mobile/Helpers/IconFont.cs
@@ -0,0 +1,35 @@
+namespace Trenios.Mobile.Helpers;
+
+/// <summary>
+/// FluentUI System Icons Regular glyph constants.
+/// Font: FluentSystemIcons-Regular.ttf, alias "FluentIcons"
+/// </summary>
+public static class IconFont
+{
+    public const string Add = "\uF119";           // ic_fluent_add_20_regular (61705)
+    public const string Subtract = "\uEBD0";      // ic_fluent_subtract_20_regular (60368)
+    public const string ArrowLeft = "\uF17B";      // ic_fluent_arrow_left_20_regular (61787)
+    public const string ArrowRight = "\uF1A1";     // ic_fluent_arrow_right_20_regular (61825)
+    public const string ArrowSync = "\uF1B0";      // ic_fluent_arrow_sync_20_regular (61840)
+    public const string Cart = "\uF27B";           // ic_fluent_cart_24_regular (62059)
+    public const string Checkmark = "\uF294";      // ic_fluent_checkmark_20_regular (62100)
+    public const string ChevronDown = "\uF2A3";    // ic_fluent_chevron_down_20_regular (62115)
+    public const string ChevronRight = "\uF2B0";   // ic_fluent_chevron_right_20_regular (62128)
+    public const string Clock = "\uF2CD";          // ic_fluent_clock_20_regular (62173)
+    public const string Delete = "\uF34C";         // ic_fluent_delete_20_regular (62284)
+    public const string Dismiss = "\uF379";        // ic_fluent_dismiss_20_regular (62313)
+    public const string Fire = "\uEF59";           // ic_fluent_fire_20_regular (61289)
+    public const string Food = "\uF453";           // ic_fluent_food_20_regular (62531)
+    public const string Globe = "\uE6F1";          // ic_fluent_globe_16_regular (59057)
+    public const string Home = "\uF4B0";           // ic_fluent_home_20_regular (62592)
+    public const string LockClosed = "\uE79F";     // ic_fluent_lock_closed_20_regular (59279)
+    public const string Person = "\uF5BD";         // ic_fluent_person_20_regular (62909)
+    public const string Receipt = "\uEA1E";        // ic_fluent_receipt_20_regular (59902)
+    public const string Search = "\uF68F";         // ic_fluent_search_20_regular (63119)
+    public const string ShoppingBag = "\uF79F";    // ic_fluent_shopping_bag_20_regular (63343)
+    public const string SignOut = "\uEB1A";        // ic_fluent_sign_out_20_regular (60154)
+    public const string Star = "\uF72F";           // ic_fluent_star_20_regular (63247)
+    public const string Table = "\uF7BD";          // ic_fluent_table_20_regular (63325)
+    public const string VehicleCar = "\uF858";     // ic_fluent_vehicle_car_20_regular (63560)
+    public const string Warning = "\uF879";        // ic_fluent_warning_20_regular (63593)
+}
